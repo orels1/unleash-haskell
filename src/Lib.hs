c8c39fbc5a91c54292a274345aaa1d898e529667
orels1 <orel-s1@yandex.ru>
2017-12-04 21:17:24 +0300

    2x'l' in 'Haskell' 😠

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,6 @@ module Lib
     ( someFunc
     ) where
 
-someFunc = let (name, lang) = ("X-Team", "haskel") in
+someFunc = let (name, lang) = ("X-Team", "haskell") in
     let message = "Hi from " ++ name ++ " on " ++ lang in
         putStrLn message
